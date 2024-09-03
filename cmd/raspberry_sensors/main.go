c46910ef67fe79b0c8497a198e3fc0e01c4d9493
Philippe Cotte <cottephi@gmail.com>
2024-09-03 11:58:49 +0200

    feat(lint): reorganizing

--- cmd/raspberry_sensors/main.go
+++ cmd/raspberry_sensors/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"raspberry_sensors/internal/sensors"
+	"raspberry_sensors/internal/api"
 	"log"
 	"periph.io/x/host/v3"
 )
@@ -14,17 +16,17 @@ func main() {
 	ctrlChan := make(chan bool)
 	defer close(ctrlChan)
 
-	sensors:= []*Sensor {
-		&NewSME280Sensor("/dev/i2c-1", 0x76).Sensor,
+	sensors_slice:= []*sensors.Sensor {
+		&sensors.NewSME280Sensor("/dev/i2c-1", 0x76).Sensor,
 	}
 
-	for _, sensor := range sensors {
+	for _, sensor := range sensors_slice {
 		sensor.Start()
 		defer sensor.Stop()
 		go sensor.Monitor(ctrlChan)
 	}
 
-	server := NewServer(ctrlChan)
+	server := api.NewServer(ctrlChan)
 	server.Start(8080)
 	
 }
