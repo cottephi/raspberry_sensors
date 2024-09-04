6a15562e423011e2455d497193c79bf46289b214
Philippe Cotte <cottephi@gmail.com>
2024-09-04 15:33:50 +0200

    chore(main): moved functions around a bit

--- cmd/raspberry_sensors/main.go
+++ cmd/raspberry_sensors/main.go
@@ -1,16 +1,8 @@
 package main
 
 import (
-	"raspberry_sensors/internal/sensors"
-	"raspberry_sensors/internal/api"
 	"raspberry_sensors/internal/utils"
-	"raspberry_sensors/internal/logger"
-	"periph.io/x/host/v3"
-	"github.com/influxdata/influxdb-client-go/v2"
-	"os"
 	"flag"
-	"fmt"
-	"time"
 )
 
 func main() {
@@ -20,61 +12,22 @@ func main() {
 	dryRun := flag.Bool("d", false, "Dry run. If True, will not write to DB")
 	flag.Parse()
 
-	err := logger.InitGlobalLogger(*logFile, logger.DebugLevel)
-	if err != nil {
-			fmt.Printf("Failed to initialize logger: %v", err)
-			os.Exit(1)
-	}
-	defer logger.GlobalLogger.Close()
+	utils.StartLogger(*logFile)
 
-	if _, err := host.Init(); err != nil {
-		logger.GlobalLogger.Fatal(err)
-	}
+	influxClient := utils.StartDB()
+	defer (*influxClient).Close()
 
-	INFLUXDB_SENSOR_TOKEN := os.Getenv("INFLUXDB_SENSOR_TOKEN")
+	controlChannels, sensors_slice := utils.StartSensors(influxClient, *dryRun)
 
-	if INFLUXDB_SENSOR_TOKEN == "" {
-		logger.GlobalLogger.Fatal("Environment variable 'INFLUXDB_SENSOR_TOKEN' not found or empty")
-	}
-
-	logger.GlobalLogger.Debug("Creating InfluxDB client...")
-	influxClient := influxdb2.NewClient("http://localhost:8086", INFLUXDB_SENSOR_TOKEN)
-	defer influxClient.Close()
-	logger.GlobalLogger.Debug("...ok")
-
-	logger.GlobalLogger.Debug("Creating Sensors...")
-	sensors_slice := []*sensors.Sensor {
-		&sensors.NewSME280Sensor("/dev/i2c-1", 0x76, influxClient, "raspberry", "seconds", *dryRun).Sensor,
-	}
-	logger.GlobalLogger.Debug("...ok")
-
-	var controlChannels [][2]chan bool
-
-	for _, sensor := range sensors_slice {
-		logger.GlobalLogger.Debugf("Starting sensor %s...", sensor.Name)
-		sensorControlChannels := [2]chan bool{make(chan bool), make(chan bool)}
-		defer close(sensorControlChannels[0])
-		defer close(sensorControlChannels[1])
-
-		sensor.Start()
+	for i, sensor := range sensors_slice {
+		defer close(controlChannels[i][0])
+		defer close(controlChannels[i][1])
 		defer sensor.Stop()
-
-		go sensor.Monitor(sensorControlChannels)
-		controlChannels = append(controlChannels, sensorControlChannels)
-		logger.GlobalLogger.Debug("...ok")
 	}
 
-	logger.GlobalLogger.Debug("Starting server...")
-	server := api.NewServer(controlChannels)
-	go server.Start(8080)
-	logger.GlobalLogger.Debug("...ok")
+	server := utils.StartServer(controlChannels)
 
-	if *start {
-		logger.GlobalLogger.Debug("Sending data acquisition start signal right away")
-		err := utils.QueryWithRetry("http://localhost:8080/sensors/start", 5 * time.Second)
-    if err != nil {
-			logger.GlobalLogger.Fatalf("Failed to start sensors: %v", err)
-    }
-	}
+	utils.UseStartFlag(*start)
+
 	utils.WaitForExitSignal(server)
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,15 +1,88 @@
 package utils
 
 import (
-	"time"
+	"fmt"
 	"net/http"
-	"strings"
-	"raspberry_sensors/internal/api"
-	"raspberry_sensors/internal/logger"
 	"os"
 	"os/signal"
+	"raspberry_sensors/internal/api"
+	"raspberry_sensors/internal/logger"
+	"raspberry_sensors/internal/sensors"
+	"strings"
 	"syscall"
+	"time"
+	"github.com/influxdata/influxdb-client-go/v2"
+	"periph.io/x/host/v3"
 )
+
+
+
+func StartLogger(logFile string) {
+	err := logger.InitGlobalLogger(logFile, logger.DebugLevel)
+	if err != nil {
+			fmt.Printf("Failed to initialize logger: %v", err)
+			os.Exit(1)
+	}
+	defer logger.GlobalLogger.Close()
+
+	if _, err := host.Init(); err != nil {
+		logger.GlobalLogger.Fatal(err)
+	}
+}
+
+func StartDB() *influxdb2.Client {
+
+	INFLUXDB_SENSOR_TOKEN := os.Getenv("INFLUXDB_SENSOR_TOKEN")
+
+	if INFLUXDB_SENSOR_TOKEN == "" {
+		logger.GlobalLogger.Fatal("Environment variable 'INFLUXDB_SENSOR_TOKEN' not found or empty")
+	}
+
+	logger.GlobalLogger.Debug("Creating InfluxDB client...")
+	influxClient := influxdb2.NewClient("http://localhost:8086", INFLUXDB_SENSOR_TOKEN)
+	logger.GlobalLogger.Debug("...ok")
+	return &influxClient
+}
+
+func StartSensors(influxClient *influxdb2.Client, dryRun bool) ([][2]chan bool, []*sensors.Sensor) {
+	logger.GlobalLogger.Debug("Creating Sensors...")
+	sensors_slice := []*sensors.Sensor {
+		&sensors.NewSME280Sensor("/dev/i2c-1", 0x76, *influxClient, "raspberry", "seconds", dryRun).Sensor,
+	}
+	logger.GlobalLogger.Debug("...ok")
+
+	var controlChannels [][2]chan bool
+
+	for _, sensor := range sensors_slice {
+		logger.GlobalLogger.Debugf("Starting sensor %s...", sensor.Name)
+		sensorControlChannels := [2]chan bool{make(chan bool), make(chan bool)}
+
+		sensor.Start()
+
+		go sensor.Monitor(sensorControlChannels)
+		controlChannels = append(controlChannels, sensorControlChannels)
+		logger.GlobalLogger.Debug("...ok")
+	}
+	return controlChannels, sensors_slice
+}
+
+func StartServer(controlChannels [][2]chan bool) *api.Server {
+	logger.GlobalLogger.Debug("Starting server...")
+	server := api.NewServer(controlChannels)
+	go server.Start(8080)
+	logger.GlobalLogger.Debug("...ok")
+	return server
+}
+
+func UseStartFlag(start bool) {
+	if start {
+		logger.GlobalLogger.Debug("Sending data acquisition start signal right away")
+		err := QueryWithRetry("http://localhost:8080/sensors/start", 5 * time.Second)
+    if err != nil {
+			logger.GlobalLogger.Fatalf("Failed to start sensors: %v", err)
+    }
+	}
+}
 
 func QueryWithRetry(url string, timeout time.Duration) error {
     // Deadline is the maximum time we allow for retries
